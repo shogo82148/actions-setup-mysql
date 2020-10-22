d230222ce86ca62f71ed062153249bdc68482696
Ichinose Shogo <shogo82148@gmail.com>
2020-10-22 23:45:54 +0900

    build MySQL

--- .github/build-mysql.sh
+++ .github/build-mysql.sh
@@ -2,4 +2,15 @@
 
 MYSQL_VERSION=$1
 
-echo "$MYSQL_VERSION"
+set -eux
+mkdir -p working
+cd working
+
+# download MySQL souce
+curl -sSL "https://github.com/mysql/mysql-server/archive/mysql-$MYSQL_VERSION.tar.gz" -o mysql.tar.gz
+tar zxvf mysql.tar.gz
+
+mkdir build
+cd build
+cmake "../mysql-server-mysql-$MYSQL_VERSION" -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost
+make "-j$(nproc)"
