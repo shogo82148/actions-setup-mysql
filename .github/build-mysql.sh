6c14f9b175b575708290da6418a4cf251517c9e2
Ichinose Shogo <shogo82148@gmail.com>
2020-10-23 09:04:50 +0900

    make install

--- .github/build-mysql.sh
+++ .github/build-mysql.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 MYSQL_VERSION=$1
+ROOT=$(cd "$(dirname "$0")" && pwd)
+: "${RUNNER_TEMP:=$ROOT/working}"
 
 set -eux
-mkdir -p working
-cd working
+mkdir -p "$RUNNER_TEMP"
+cd "$RUNNER_TEMP"
 
 # download MySQL souce
 curl -sSL "https://github.com/mysql/mysql-server/archive/mysql-$MYSQL_VERSION.tar.gz" -o mysql.tar.gz
@@ -21,7 +23,9 @@ if command -v nproc > /dev/null; then
     JOBS=$(nproc || echo "$JOBS")
 fi
 
+# build
 mkdir build
 cd build
-cmake "../mysql-server-mysql-$MYSQL_VERSION" -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost
+cmake "../mysql-server-mysql-$MYSQL_VERSION" -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost -DCMAKE_INSTALL_PREFIX="$RUNNER_TEMP/dist"
 make "-j$JOBS"
+make install
