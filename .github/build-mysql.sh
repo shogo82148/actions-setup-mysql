97a5929a104cd52df597212bd479260b9f6023de
Ichinose Shogo <shogo82148@gmail.com>
2020-10-24 11:28:22 +0900

    use sub-shell

--- .github/build-mysql.sh
+++ .github/build-mysql.sh
@@ -6,43 +6,60 @@ ROOT=$(cd "$(dirname "$0")" && pwd)
 : "${RUNNER_TOOL_CACHE:=$ROOT/dist}"
 PREFIX=$RUNNER_TOOL_CACHE/mysql/$MYSQL_VERSION/x64
 
-set -eux
-mkdir -p "$RUNNER_TEMP"
-cd "$RUNNER_TEMP"
-
 echo "::group::download MySQL souce"
-curl -sSL "https://github.com/mysql/mysql-server/archive/mysql-$MYSQL_VERSION.tar.gz" -o mysql.tar.gz
+(
+    set -eux
+    mkdir -p "$RUNNER_TEMP"
+    cd "$RUNNER_TEMP"
+    rm -rf ./*
+    curl -sSL "https://github.com/mysql/mysql-server/archive/mysql-$MYSQL_VERSION.tar.gz" -o mysql-src.tar.gz
+)
 echo "::endgroup::"
 
 echo "::group::extract"
-tar zxvf mysql.tar.gz
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    tar zxvf mysql-src.tar.gz
+)
 echo "::endgroup::"
 
 echo "::group::build"
-# detect the number of CPU Core
-JOBS=1
-if command -v sysctl > /dev/null; then
-    # on macOX
-    JOBS=$(sysctl -n hw.logicalcpu_max || echo "$JOBS")
-fi
-if command -v nproc > /dev/null; then
-    # on Linux
-    JOBS=$(nproc || echo "$JOBS")
-fi
+(
+    set -eux
+    # detect the number of CPU Core
+    JOBS=1
+    if command -v sysctl > /dev/null; then
+        # on macOX
+        JOBS=$(sysctl -n hw.logicalcpu_max || echo "$JOBS")
+    fi
+    if command -v nproc > /dev/null; then
+        # on Linux
+        JOBS=$(nproc || echo "$JOBS")
+    fi
 
-# build
-mkdir build
-cd build
-cmake "../mysql-server-mysql-$MYSQL_VERSION" -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost -DCMAKE_INSTALL_PREFIX="$PREFIX"
-make "-j$JOBS"
+    # build
+    cd "$RUNNER_TEMP"
+    mkdir build
+    cd build
+    cmake "../mysql-server-mysql-$MYSQL_VERSION" -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost -DCMAKE_INSTALL_PREFIX="$PREFIX"
+    make "-j$JOBS"
+)
 echo "::endgroup::"
 
 echo "::group::install"
-make install
+(
+    set -eux
+    cd "$RUNNER_TEMP/build"
+    make install
+)
 echo "::endgroup::"
 
 # archive
 echo "::group::archive"
-cd "$PREFIX"
-tar vczf "$RUNNER_TEMP/mysql.tar.gz" .
+(
+    set -eux
+    cd "$PREFIX"
+    tar vczf "$RUNNER_TEMP/mysql.tar.gz" .
+)
 echo "::endgroup::"
