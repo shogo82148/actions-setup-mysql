040225874aa4281ebacc27a0ce3309f0d05859c0
Ichinose Shogo <shogo82148@gmail.com>
2020-10-24 11:00:50 +0900

    grouping

--- .github/build-mysql.sh
+++ .github/build-mysql.sh
@@ -3,15 +3,22 @@
 MYSQL_VERSION=$1
 ROOT=$(cd "$(dirname "$0")" && pwd)
 : "${RUNNER_TEMP:=$ROOT/working}"
+: "${RUNNER_TOOL_CACHE:=$ROOT/dist}"
+PREFIX=$RUNNER_TOOL_CACHE/mysql/$MYSQL_VERSION/x64
 
 set -eux
 mkdir -p "$RUNNER_TEMP"
 cd "$RUNNER_TEMP"
 
-# download MySQL souce
+echo "::group::download MySQL souce"
 curl -sSL "https://github.com/mysql/mysql-server/archive/mysql-$MYSQL_VERSION.tar.gz" -o mysql.tar.gz
-tar zxvf mysql.tar.gz
+echo "::endgroup::"
 
+echo "::group::extract"
+tar zxvf mysql.tar.gz
+echo "::endgroup::"
+
+echo "::group::build"
 # detect the number of CPU Core
 JOBS=1
 if command -v sysctl > /dev/null; then
@@ -26,10 +33,16 @@ fi
 # build
 mkdir build
 cd build
-cmake "../mysql-server-mysql-$MYSQL_VERSION" -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost -DCMAKE_INSTALL_PREFIX="$RUNNER_TEMP/dist"
+cmake "../mysql-server-mysql-$MYSQL_VERSION" -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost -DCMAKE_INSTALL_PREFIX="$PREFIX"
 make "-j$JOBS"
+echo "::endgroup::"
+
+echo "::group::install"
 make install
+echo "::endgroup::"
 
 # archive
-cd "$RUNNER_TEMP/dist"
-tar czf "../mysql.tar.gz" .
+echo "::group::archive"
+cd "$PREFIX"
+tar vczf "$RUNNER_TEMP/mysql.tar.gz" .
+echo "::endgroup::"
