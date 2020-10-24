bdd4f6fd86a015e8a9607fac8df8bfedf0d5ff6a
Ichinose Shogo <shogo82148@gmail.com>
2020-10-24 22:24:24 +0900

    install OpenSSL

--- .github/build-mysql.sh
+++ .github/build-mysql.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 MYSQL_VERSION=$1
-LIBRESSL_VERSION=3.2.2
+OPENSSL_VERSION=1_1_1h
 ROOT=$(cd "$(dirname "$0")" && pwd)
 : "${RUNNER_TEMP:=$ROOT/working}"
 : "${RUNNER_TOOL_CACHE:=$RUNNER_TEMP/dist}"
@@ -18,12 +20,41 @@ if command -v nproc > /dev/null; then
     JOBS=$(nproc || echo "$JOBS")
 fi
 
+mkdir -p "$RUNNER_TEMP"
+cd "$RUNNER_TEMP"
+rm -rf ./*
+
+# system SSL/TLS library is too old. so we use custom build.
+echo "::group::download OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    curl -sSL "https://github.com/openssl/openssl/archive/OpenSSL_$OPENSSL_VERSION.tar.gz" -o openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::extract OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    tar zxvf openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::build OpenSSL"
+(
+    set -eux
+    cd "$RUNNER_TEMP/openssl-OpenSSL_$OPENSSL_VERSION"
+    ./Configure --prefix="$PREFIX" gcc
+    make "-j$JOBS"
+    make install_sw
+)
+echo "::endgroup::"
+
 echo "::group::download MySQL source"
 (
     set -eux
-    mkdir -p "$RUNNER_TEMP"
     cd "$RUNNER_TEMP"
-    rm -rf ./*
     curl -sSL "https://github.com/mysql/mysql-server/archive/mysql-$MYSQL_VERSION.tar.gz" -o mysql-src.tar.gz
 )
 echo "::endgroup::"
@@ -33,34 +64,6 @@ echo "::group::extract MySQL source"
     set -eux
     cd "$RUNNER_TEMP"
     tar zxvf mysql-src.tar.gz
-)
-echo "::endgroup::"
-
-# system SSL/TLS library is too old. so we use custom build.
-echo "::group::download LibreSSL source"
-(
-    set -eux
-    mkdir -p "$RUNNER_TEMP"
-    cd "$RUNNER_TEMP"
-    curl -sSL "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-$LIBRESSL_VERSION.tar.gz" -o libressl.tar.gz
-)
-echo "::endgroup::"
-
-echo "::group::extract LibreSSL source"
-(
-    set -eux
-    cd "$RUNNER_TEMP"
-    tar zxvf libressl.tar.gz
-)
-echo "::endgroup::"
-
-echo "::group::build LibreSSL"
-(
-    set -eux
-    cd "$RUNNER_TEMP/libressl-$LIBRESSL_VERSION"
-    ./configure --prefix="$PREFIX"
-    make "-j$JOBS"
-    make install
 )
 echo "::endgroup::"
 
