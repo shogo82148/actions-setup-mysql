23d650adcf68a77f04bd6b14e4b1b86fb8ba4f5c
Ichinose Shogo <shogo82148@gmail.com>
2020-10-25 21:46:30 +0900

    Fix building MySQL error (#12)
    * enable pull request trigger for testing
    
    * build with GCC
    
    * ./config shared
    https://stackoverflow.com/questions/1147890/relocation-r-x86-64-32-against-a-local-symbol-error
    
    * --openssldir="$PREFIX/ssl"
    
    * --libdir="$PREFIX/lib"
    
    * fix shared option
    
    * add pic option
    
    * fix target
    
    * supress warnings
    
    * remove gcc
    
    * seperate scripts
    
    * use default configure

--- .github/build-mysql-darwin.sh
+++ .github/build-mysql-darwin.sh
@@ -10,15 +10,7 @@ ROOT=$(cd "$(dirname "$0")" && pwd)
 PREFIX=$RUNNER_TOOL_CACHE/mysql/$MYSQL_VERSION/x64
 
 # detect the number of CPU Core
-JOBS=1
-if command -v sysctl > /dev/null; then
-    # on macOX
-    JOBS=$(sysctl -n hw.logicalcpu_max || echo "$JOBS")
-fi
-if command -v nproc > /dev/null; then
-    # on Linux
-    JOBS=$(nproc || echo "$JOBS")
-fi
+JOBS=$(sysctl -n hw.logicalcpu_max)
 
 mkdir -p "$RUNNER_TEMP"
 cd "$RUNNER_TEMP"
@@ -45,7 +37,7 @@ echo "::group::build OpenSSL"
 (
     set -eux
     cd "$RUNNER_TEMP/openssl-OpenSSL_$OPENSSL_VERSION"
-    ./Configure --prefix="$PREFIX" clang
+    ./Configure --prefix="$PREFIX" darwin64-x86_64-cc
     make "-j$JOBS"
     make install_sw
 )
@@ -73,7 +65,7 @@ echo "::group::build MySQL"
     cd "$RUNNER_TEMP"
     mkdir build
     cd build
-    CC=clang CXX=clang++ cmake "../mysql-server-mysql-$MYSQL_VERSION" \
+    cmake "../mysql-server-mysql-$MYSQL_VERSION" \
         -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost \
         -DCMAKE_INSTALL_PREFIX="$PREFIX" \
         -DWITH_SSL="$PREFIX"
---
+++ .github/build-mysql-linux.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+set -e
+
+MYSQL_VERSION=$1
+OPENSSL_VERSION=1_1_1h
+ROOT=$(cd "$(dirname "$0")" && pwd)
+: "${RUNNER_TEMP:=$ROOT/working}"
+: "${RUNNER_TOOL_CACHE:=$RUNNER_TEMP/dist}"
+PREFIX=$RUNNER_TOOL_CACHE/mysql/$MYSQL_VERSION/x64
+
+# detect the number of CPU Core
+JOBS=$(nproc)
+
+mkdir -p "$RUNNER_TEMP"
+cd "$RUNNER_TEMP"
+rm -rf ./*
+
+# system SSL/TLS library is too old. so we use custom build.
+echo "::group::download OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    curl -sSL "https://github.com/openssl/openssl/archive/OpenSSL_$OPENSSL_VERSION.tar.gz" -o openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::extract OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    tar zxvf openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::build OpenSSL"
+(
+    set -eux
+    cd "$RUNNER_TEMP/openssl-OpenSSL_$OPENSSL_VERSION"
+    ./Configure --prefix="$PREFIX" linux-x86_64
+    make "-j$JOBS"
+    make install_sw
+)
+echo "::endgroup::"
+
+echo "::group::download MySQL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    curl -sSL "https://github.com/mysql/mysql-server/archive/mysql-$MYSQL_VERSION.tar.gz" -o mysql-src.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::extract MySQL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    tar zxvf mysql-src.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::build MySQL"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    mkdir build
+    cd build
+    cmake "../mysql-server-mysql-$MYSQL_VERSION" \
+        -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost \
+        -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+        -DWITH_SSL="$PREFIX"
+    make "-j$JOBS"
+)
+echo "::endgroup::"
+
+echo "::group::install"
+(
+    set -eux
+    cd "$RUNNER_TEMP/build"
+    make install
+)
+echo "::endgroup::"
+
+# archive
+echo "::group::archive"
+(
+    set -eux
+    cd "$PREFIX"
+    tar vczf "$RUNNER_TEMP/mysql.tar.gz" .
+)
+echo "::endgroup::"
