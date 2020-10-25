2014492afa684a088c5e2a6611c04cb68d14ef36
Ichinose Shogo <shogo82148@gmail.com>
2020-10-25 16:45:14 +0900

    add pic option

--- .github/build-mysql.sh
+++ .github/build-mysql.sh
@@ -45,7 +45,7 @@ echo "::group::build OpenSSL"
 (
     set -eux
     cd "$RUNNER_TEMP/openssl-OpenSSL_$OPENSSL_VERSION"
-    ./Configure --prefix="$PREFIX" --openssldir="$PREFIX/ssl" --libdir="$PREFIX/lib" gcc shared
+    ./Configure --prefix="$PREFIX" --openssldir="$PREFIX/ssl" --libdir="$PREFIX/lib" gcc shared pic
     make "-j$JOBS"
     make install_sw
 )
