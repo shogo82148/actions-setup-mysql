aad94c3e5dfa83d8f6e32fc1c67600afd71b76b5
Ichinose Shogo <shogo82148@gmail.com>
2020-10-26 18:55:00 +0900

    start MySQL database

--- src/starter.ts
+++ src/starter.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs'
 import * as os from 'os'
 import * as path from 'path'
+import * as child_process from 'child_process'
 import * as core from '@actions/core'
 import * as io from '@actions/io'
 import * as exec from '@actions/exec'
@@ -16,6 +17,7 @@ export async function startMySQL(mysqlPath: string) {
 
   const myCnf = `
 [mysqld]
+lc-messages-dir=/Users/shogoichinose/src/github.com/shogo82148/actions-setup-mysql/__tests__/runner/tools/mysql/5.6.50/x64/share
 socket=${baseDir}${sep}tmp${sep}mysql.sock
 datadir=${baseDir}${sep}var
 pid-file=${baseDir}${sep}tmp${sep}mysqld.pid
@@ -28,13 +30,26 @@ tmpdir=${baseDir}${sep}tmp
     `--defaults-file=${baseDir}${sep}etc${sep}my.cnf`,
     `--basedir=${mysqlPath}`
   ])
+
+  core.debug('start MySQL database')
+  const out = fs.openSync(path.join(baseDir, 'tmp', 'mysqld.log'), 'a')
+  const err = fs.openSync(path.join(baseDir, 'tmp', 'mysqld.log'), 'a')
+  const subprocess = child_process.spawn(
+    path.join(mysqlPath, 'bin', 'mysqld'),
+    [`--defaults-file=${baseDir}${sep}etc${sep}my.cnf`, '--user=root'],
+    {
+      detached: true,
+      stdio: ['ignore', out, err]
+    }
+  )
+  subprocess.unref()
 }
 
 function mkdtemp(): Promise<string> {
   const tmp = process.env['RUNNER_TEMP'] || os.tmpdir()
   const sep = path.sep
   return new Promise(function (resolve, reject) {
-    fs.mkdtemp(`${tmp}${sep}`, (err, dir) => {
+    fs.mkdtemp(`${tmp}${sep}actions-setup-mysql-`, (err, dir) => {
       if (err) {
         reject(err)
         return
