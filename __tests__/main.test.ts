196947691437fba4bb7ec612352979ee151d12e0
Ichinose Shogo <shogo82148@gmail.com>
2020-10-29 20:51:54 +0900

    add async

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -14,7 +14,7 @@ import * as installer from '../src/installer'
 import * as starter from '../src/starter'
 import * as cleanup from '../src/cleanup'
 
-describe('installer tests', () => {
+describe('installer tests', async () => {
   beforeAll(async () => {
     await io.rmRF(toolDir)
     await io.rmRF(tempDir)
