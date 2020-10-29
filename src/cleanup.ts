0893d2f7b89467fc4581679658aec52c2a0adc7f
Ichinose Shogo <shogo82148@gmail.com>
2020-10-29 20:44:35 +0900

    show shutdown log

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -1,10 +1,13 @@
 import * as path from 'path'
 import * as starter from './starter'
+import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import * as io from '@actions/io'
 
 export async function shutdownMySQL(state: starter.MySQLState) {
-  const shutdown = path.join(__dirname, '..', 'scripts', 'shutdown.sh')
-  await exec.exec(shutdown, [`${state.pid}`])
-  await io.rmRF(state.baseDir)
+  core.group('shutdown MySQL Server', async () => {
+    const shutdown = path.join(__dirname, '..', 'scripts', 'shutdown.sh')
+    await exec.exec(shutdown, [`${state.pid}`])
+    await io.rmRF(state.baseDir)  
+  })
 }
