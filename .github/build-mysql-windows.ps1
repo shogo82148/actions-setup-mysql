e8c7c6a85c97a220be12ad5830c43b61390fde30
Ichinose Shogo <shogo82148@gmail.com>
2020-10-30 11:46:25 +0900

    use Set-Item cmdlet

--- .github/build-mysql-windows.ps1
+++ .github/build-mysql-windows.ps1
@@ -15,7 +15,7 @@ Remove-Item -Path * -Recurse -Force
 cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat`" x64 && set > %temp%\vcvars.txt"
 Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
     if ($_ -match "^(.*?)=(.*)$") {
-        Set-Content "env:\$($matches[1])" $matches[2]
+        Set-Item -Path "env:$($matches[1])" $matches[2]
         Write-Host "::debug::$($matches[1])=$($matches[2])"
     }
 }
