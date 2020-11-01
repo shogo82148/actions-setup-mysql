c0d83690e8f2de29c2b86d21cd7181bd4cfe2994
Ichinose Shogo <shogo82148@gmail.com>
2020-11-01 16:51:05 +0900

    use default config

--- .github/build-mysql-windows.ps1
+++ .github/build-mysql-windows.ps1
@@ -99,13 +99,13 @@ cmake ( Join-Path $RUNNER_TEMP "mysql-server-mysql-$MYSQL_VERSION" ) `
     -DWITH_SSL="$PREFIX" `
     -DCMAKE_BUILD_TYPE=MinSizeRel
 
-devenv MySQL.sln
+devenv MySQL.sln /build
 Write-Host "::endgroup::"
 
 Write-Host "::group::install"
 Set-Location "$RUNNER_TEMP\build"
-devenv MySQL.sln /project initial_database
-devenv MySQL.sln /project package
+devenv MySQL.sln /build /project initial_database
+devenv MySQL.sln /build /project package
 Write-Host "::endgroup::"
 
 
