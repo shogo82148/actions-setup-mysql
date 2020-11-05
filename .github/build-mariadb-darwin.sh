22dddc838ce1f4990bb2da0b36315a1d67492d65
Ichinose Shogo <shogo82148@gmail.com>
2020-11-05 20:24:03 +0900

    build MariaDB (#40)
    * build MariaDB
    
    * with MariaDB 5.5.x, use bundled SSL Library
    
    * fix source path
    
    * use bundled ssl library
    
    * Revert "use bundled ssl library"
    
    This reverts commit 0006caa59079e206c65175e6ebf4e5c89abd3ffe.
    
    * disable plugins
    
    * use bundled ssl library with MariaDB 5.5.x
    
    * use GCC
    
    * Revert "use GCC"
    
    This reverts commit 0a3c8fc3c4efd03141b26128254ccc624a0897cb.
    
    * fix path of zip
    
    * drop MariaDB 5.5.x
    
    * 10.0.x only supports WITHOUT_PLUGIN_TOKUDB=1 style
    
    * drop 10.2.x and 10.1.x

---
+++ .github/build-mariadb-darwin.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+set -e
+
+MARIADB_VERSION=$1
+OPENSSL_VERSION=1_1_1h
+ROOT=$(cd "$(dirname "$0")" && pwd)
+: "${RUNNER_TEMP:=$ROOT/working}"
+: "${RUNNER_TOOL_CACHE:=$RUNNER_TEMP/dist}"
+PREFIX=$RUNNER_TOOL_CACHE/mariadb/$MARIADB_VERSION/x64
+
+# detect the number of CPU Core
+JOBS=$(sysctl -n hw.logicalcpu_max)
+
+mkdir -p "$RUNNER_TEMP"
+cd "$RUNNER_TEMP"
+rm -rf ./*
+
+# system SSL/TLS library is too old. so we use custom build.
+echo "::group::download OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    curl -sSL "https://github.com/openssl/openssl/archive/OpenSSL_$OPENSSL_VERSION.tar.gz" -o openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::extract OpenSSL source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    tar zxvf openssl.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::build OpenSSL"
+(
+    set -eux
+    cd "$RUNNER_TEMP/openssl-OpenSSL_$OPENSSL_VERSION"
+    ./Configure --prefix="$PREFIX" darwin64-x86_64-cc
+    make "-j$JOBS"
+    make install_sw
+)
+echo "::endgroup::"
+
+echo "::group::download MariaDB source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    curl -sSL "https://downloads.mariadb.com/MariaDB/mariadb-$MARIADB_VERSION/source/mariadb-$MARIADB_VERSION.tar.gz" -o mariadb-src.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::extract MariaDB source"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    tar zxvf mariadb-src.tar.gz
+)
+echo "::endgroup::"
+
+echo "::group::build MariaDB"
+(
+    set -eux
+    cd "$RUNNER_TEMP"
+    mkdir build
+    cd build
+    cmake "../mariadb-$MARIADB_VERSION" \
+        -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost \
+        -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+        -DWITH_SSL="$PREFIX" -DPLUGIN_TOKUDB=NO -DPLUGIN_MROONGA=NO -DPLUGIN_SPIDER=NO -DPLUGIN_OQGRAPH=NO -DPLUGIN_PERFSCHEMA=NO -DPLUGIN_SPHINX=NO -DPLUGIN_ARCHIVE=NO -DPLUGIN_ROCKSDB=NO
+    make "-j$JOBS"
+)
+echo "::endgroup::"
+
+echo "::group::install"
+(
+    set -eux
+    cd "$RUNNER_TEMP/build"
+    make install
+)
+echo "::endgroup::"
+
+# archive
+echo "::group::archive"
+(
+    set -eux
+    cd "$PREFIX"
+
+    # remove extra files
+    rm -rf ./man
+    rm -rf ./mysql-test
+    rm -rf ./sql-bench
+
+    tar Jvcf "$RUNNER_TEMP/mariadb.tar.xz" .
+)
+echo "::endgroup::"
