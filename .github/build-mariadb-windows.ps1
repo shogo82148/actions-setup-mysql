469c2184a402a7979e51e4c42dcb4d38678e06bb
Ichinose Shogo <shogo82148@gmail.com>
2021-02-25 18:55:20 +0900

    bump OpenSSL v1.1.1j (#137)

--- .github/build-mariadb-windows.ps1
+++ .github/build-mariadb-windows.ps1
@@ -1,5 +1,5 @@
 Param($MARIADB_VERSION)
-$OPENSSL_VERSION = "1_1_1i"
+$OPENSSL_VERSION = "1_1_1j"
 $ROOT = Split-Path -Parent $MyInvocation.MyCommand.Path
 $RUNNER_TEMP = $env:RUNNER_TEMP
 if ($null -eq $RUNNER_TEMP) {
