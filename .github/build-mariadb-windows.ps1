8150d36c82e91fc9fd0c82e9c62ad8b2e35c04b0
Ichinose Shogo <shogo82148@gmail.com>
2021-06-26 22:43:16 +0900

    fix the path for the patches

--- .github/build-mariadb-windows.ps1
+++ .github/build-mariadb-windows.ps1
@@ -92,7 +92,7 @@ Invoke-WebRequest "https://downloads.mariadb.com/MariaDB/mariadb-$MARIADB_VERSIO
 Write-Host "Untar..."
 tar zxvf mariadb-src.tar.gz
 Remove-Item -Path "mariadb-src.tar.gz"
-if (Test-Path ( Join-Path $ROOT .. "patches" "mysql" $MARIADB_VERSION )) {
+if (Test-Path ( Join-Path $ROOT .. "patches" "mariadb" $MARIADB_VERSION )) {
     Set-Location "mariadb-$MARIADB_VERSION"
     Get-Content ( Join-Path $ROOT .. "patches" "mariadb" $MARIADB_VERSION *.patch ) | patch -s -f -p1
 }
