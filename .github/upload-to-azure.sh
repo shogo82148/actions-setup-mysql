69222be8f497a55f27c63912661fc8af697284a3
Ichinose Shogo <shogo82148@gmail.com>
2021-08-08 17:38:32 +0900

    fix path for retry.pl

--- .github/upload-to-azure.sh
+++ .github/upload-to-azure.sh
@@ -5,9 +5,9 @@ set -eux
 ROOT=$(cd "$(dirname "$0")" && pwd)
 ACTIONS_VERSION=v$(< "$GITHUB_WORKSPACE/package.json" jq -r .version)
 
-"$ROOT/.github/retry.pl" azcopy login --service-principal --application-id "$AZCOPY_SPA_APPLICATION_ID" --tenant-id "$AZCOPY_TENANT_ID"
+"$ROOT/retry.pl" azcopy login --service-principal --application-id "$AZCOPY_SPA_APPLICATION_ID" --tenant-id "$AZCOPY_TENANT_ID"
 
 for archive in */*
 do
-    "$ROOT/.github/retry.pl" azcopy cp "$archive" "https://setupmysql.blob.core.windows.net/actions-setup-mysql/$ACTIONS_VERSION/$(dirname "$archive")"
+    "$ROOT/retry.pl" azcopy cp "$archive" "https://setupmysql.blob.core.windows.net/actions-setup-mysql/$ACTIONS_VERSION/$(dirname "$archive")"
 done
