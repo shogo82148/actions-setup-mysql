be7a4e7d3d0c52b0921bfd42053e14514094935a
Ichinose Shogo <shogo82148@gmail.com>
2021-08-08 16:49:53 +0900

    implement upload job

---
+++ .github/upload-to-azure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eux
+
+ROOT=$(cd "$(dirname "$0")" && pwd)
+ACTIONS_VERSION=v$(< "$GITHUB_WORKSPACE/package.json" jq -r .version)
+
+"$ROOT/.github/retry.pl" azcopy login --service-principal --application-id "$AZCOPY_SPA_APPLICATION_ID" --tenant-id "$AZCOPY_TENANT_ID"
+
+for archive in */*
+do
+    "$ROOT/.github/retry.pl" azcopy cp "$archive" "https://setupmysql.blob.core.windows.net/actions-setup-mysql/$ACTIONS_VERSION/$(dirname "$archive")"
+done
