0ac1e91db638a23937695deda23ff39524dab558
Ichinose Shogo <shogo82148@gmail.com>
2021-08-26 20:55:43 +0900

    bump OpenSSL v1.1.1l

--- .github/build-mysql-windows.ps1
+++ .github/build-mysql-windows.ps1
@@ -1,5 +1,5 @@
 Param($MYSQL_VERSION)
-$OPENSSL_VERSION = "1_1_1k"
+$OPENSSL_VERSION = "1_1_1l"
 $ROOT = Split-Path -Parent $MyInvocation.MyCommand.Path
 $RUNNER_TEMP = $env:RUNNER_TEMP
 if ($null -eq $RUNNER_TEMP) {
