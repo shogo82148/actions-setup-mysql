6bc2f4c720e1e80c15a129dd482b34cd63e20cd0
Ichinose Shogo <shogo82148@gmail.com>
2021-08-28 15:36:23 +0900

    fix compile errors

--- src/setup-mysql.ts
+++ src/setup-mysql.ts
@@ -26,7 +26,11 @@ async function run(): Promise<void> {
       starter.saveState(state)
     }
   } catch (error) {
-    core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    } else {
+      core.setFailed(`${error}`)
+    }
   }
 }
 
