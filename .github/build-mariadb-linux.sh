ad3e6d45d5096e8e8dd66829c9b61989a08f627a
Ichinose Shogo <shogo82148@gmail.com>
2022-08-15 17:57:32 +0900

    fix install path for MariaDB

--- .github/build-mariadb-linux.sh
+++ .github/build-mariadb-linux.sh
@@ -20,7 +20,7 @@ case "$(uname -m)" in
         exit 1
         ;;
 esac
-PREFIX=$RUNNER_TOOL_CACHE/mysql/$MARIADB_VERSION/$MARIADB_ARCH
+PREFIX=$RUNNER_TOOL_CACHE/mariadb/$MARIADB_VERSION/$MARIADB_ARCH
 
 # use latest version of gcc installed
 if command -v gcc-11 > /dev/null 2>&1; then
