810945ac210fcb6fbd86f7726a291da94f691411
Ichinose Shogo <shogo82148@gmail.com>
2022-11-11 15:41:07 +0900

    fix the version of OpenSSL

--- .github/build-mariadb-windows.ps1
+++ .github/build-mariadb-windows.ps1
@@ -1,5 +1,6 @@
 Param($MARIADB_VERSION)
-$OPENSSL_VERSION = "1_1_1s"
+$OPENSSL_VERSION1_1_1 = "1_1_1s"
+$OPENSSL_VERSION3 = "3.0.7"
 $ROOT = Split-Path -Parent $MyInvocation.MyCommand.Path
 $RUNNER_TEMP = $env:RUNNER_TEMP
 if ($null -eq $RUNNER_TEMP) {
