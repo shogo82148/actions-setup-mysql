c87732f5433903151e557c4b1087b7d7194afe31
ICHINOSE Shogo <shogo82148@gmail.com>
2024-08-31 02:17:51 +0900

    fix: Add verbose flag to startMySQL function

--- src/starter.ts
+++ src/starter.ts
@@ -196,6 +196,7 @@ export async function startMySQL(
           if (fs.existsSync(path.join(mysql.toolPath, "bin", `mariadb-admin${binExt}`))) {
             await execute(path.join(mysql.toolPath, "bin", `mariadb-admin${binExt}`), [
               `--defaults-file=${baseDir}${sep}etc${sep}my.cnf`,
+              `--verbose`,
               `--user=root`,
               `password`,
               rootPassword,
@@ -203,6 +204,7 @@ export async function startMySQL(
           } else {
             await execute(path.join(mysql.toolPath, "bin", `mysqladmin${binExt}`), [
               `--defaults-file=${baseDir}${sep}etc${sep}my.cnf`,
+              `--verbose`,
               `--user=root`,
               `password`,
               rootPassword,
