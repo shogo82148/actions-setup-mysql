9127311d7a0b8f7e7ef1279464c8105af725c056
ICHINOSE Shogo <shogo82148@gmail.com>
2024-08-31 03:46:11 +0900

    Revert "chore: Configure TLS/SSL for MySQL client connection"
    This reverts commit d2c2a7f184eeb2848e3a2b6df5f433d92a8076be.

--- src/starter.ts
+++ src/starter.ts
@@ -145,7 +145,6 @@ export async function startMySQL(
     config["mysqld"]["ssl_ca"] ||= path.join(baseDir, "var", "ca.pem");
     config["mysqld"]["ssl_cert"] ||= path.join(baseDir, "var", "server-cert.pem");
     config["mysqld"]["ssl_key"] ||= path.join(baseDir, "var", "server-key.pem");
-    config["client"]["ssl_ca"] ||= config["mysqld"]["ssl_ca"];
 
     // configure my.cnf
     core.info(`add TLS/SSL setting into my.cnf`);
