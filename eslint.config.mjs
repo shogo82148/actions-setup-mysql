1c3ead7f493dae2a6c4ca644892e96bbe193e5bd
ICHINOSE Shogo <shogo82148@gmail.com>
2024-08-31 14:10:35 +0900

    migrate eslintignore to eslint.config.mjs (#1131)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,6 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended);
+export default tseslint.config(eslint.configs.recommended, {
+  ignores: ["dist/", "lib/", "node_modules/", ".github/", "jest.config.js"],
+});
