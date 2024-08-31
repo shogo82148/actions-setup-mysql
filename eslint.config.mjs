5af803cfd1f6e19866d0ac4bf0cd35ab0bd90343
ICHINOSE Shogo <shogo82148@gmail.com>
2024-08-31 13:41:14 +0900

    fix: failed to clean up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,4 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.strict, ...tseslint.configs.stylistic);
+export default tseslint.config(eslint.configs.recommended);
