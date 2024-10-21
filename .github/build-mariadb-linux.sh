c27c8f51e4d4fccf00c9a6c106b79b36a9e2851b
ICHINOSE Shogo <shogo82148@gmail.com>
2024-10-21 22:50:46 +0900

    fix MariaDB download URL.

--- .github/build-mariadb-linux.sh
+++ .github/build-mariadb-linux.sh
@@ -138,7 +138,7 @@ echo "::group::download MariaDB source"
 (
     set -eux
     cd "$RUNNER_TEMP"
-    curl --retry 3 -sSL "https://downloads.mariadb.com/MariaDB/mariadb-$MARIADB_VERSION/source/mariadb-$MARIADB_VERSION.tar.gz" -o mariadb-src.tar.gz
+    curl --retry 3 -sSL "https://github.com/MariaDB/server/archive/refs/tags/mariadb-$MARIADB_VERSION.tar.gz" -o mariadb-src.tar.gz
 )
 echo "::endgroup::"
 
